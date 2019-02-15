6809d81d29436996fd5734c0a86c4bb3108446c0
Valentina Kononenko <valentina@kononenko.net>
2019-02-14 21:43:42 -0500

    ?

--- src/com/company/BA_comparator.java
+++ src/com/company/BA_comparator.java
@@ -1,31 +1,6 @@
 package com.company;
 import java.util.*;
 
-/* public class BA_comparator implements Comparator {
-
-    public int compare(Object firstObject, Object secondObject) {
-
-        BankAccount ba1 = (BankAccount)firstObject;
-        BankAccount ba2 = (BankAccount)secondObject;
-
-        int retValue;
-        if (ba1.balance < ba2.balance){
-            retValue = -1;
-        }
-        else {
-
-            if (ba1.balance > ba2.balance) {
-                retValue = 1;
-            }
-            else {
-                retValue = 0;
-            }
-        }
-        return retValue;
-    }
-}
-*/
-
 public class BA_comparator implements Comparator {
 
     public int compare(Object firstObject, Object secondObject) {
